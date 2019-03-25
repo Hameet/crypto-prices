d3666380f21632cd3cf86716cc4fa9d82e534055
Hameet <hameet.singh@hotmail.com>
2019-03-25 21:32:54 +1300

    Successfully implemented React Hooks

--- src/App.js
+++ src/App.js
@@ -1,65 +1,48 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Display from './Components/Display'
 import Form from './Components/Form'
 
 const API_KEY = `89bcb9d40a3fd6b18613ef79499b757e`
 
-class App extends Component {
-  state = {
-    currency: undefined,
-    error: undefined
-  }
-  getCurrency = async e => {
-    e.preventDefault()
-    const api_call = await fetch(
-      `http://api.coinlayer.com/api/live?access_key=89bcb9d40a3fd6b18613ef79499b757e`
-    )
-    const data = await api_call.json()
-    const currency = data.rates
-    console.log('data', data)
-
-    if (data.cod == 404) {
-      this.setState({
-        temperature: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        error: 'Please make sure your inputs are valid.',
-        min_temperature: undefined,
-        max_temperature: undefined,
-        icon: null,
-        wind: undefined
+function useFetch (url, defaultResponse) {
+  const [data, setData] = useState(defaultResponse)
+  async function getDataFromAPI (url) {
+    try {
+      const res = await fetch(url)
+      const data = await res.json()
+      setData({
+        isLoading: false,
+        data
       })
-    } else {
-      this.setState({
-        currency: data.rates.BTC,
-        error: ''
-      })
+    } catch (e) {
+      console.error(e)
     }
   }
-  render () {
-    console.log(this.state.currency, 'this')
-    return (
-      <div>
-        <div className='wrapper'>
-          <div className='main'>
-            <div className='container'>
-              <div className='row'>
-                <div className='col-xs-7 form-container'>
-                  <Form getCurrency={this.getCurrency} />
-                  <Display
-                    currency={this.state.currency}
-                    error={this.state.error}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
+
+  useEffect(
+    () => {
+      getDataFromAPI(url)
+    },
+    [url]
+  )
+  return data
 }
 
-export default App
+export default function App () {
+  const apiEndpoint = `http://api.coinlayer.com/api/live?access_key=${API_KEY}`
+  const userFetchResponse = useFetch(apiEndpoint, {
+    isLoading: true,
+    data: null
+  })
+  if (!userFetchResponse.data || userFetchResponse.isLoading) {
+    return 'Loading...'
+  }
+  const { ABC, BTC } = userFetchResponse.data.rates
+  console.log('log', userFetchResponse.data.rates)
+  return (
+    <div>
+      {' '}
+      Coin: {ABC} {BTC}
+    </div>
+  )
+}
